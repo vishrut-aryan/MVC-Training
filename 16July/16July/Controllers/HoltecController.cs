263b99185948c4ad13620ff41660dd007a6bf29a
Vishrut Aryan <VAryan@holtecasia.com>
2024-07-16 10:13:39 +0530

    Add project files.

---
+++ 16July/16July/Controllers/HoltecController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _16July.Controllers
+{
+    public class HoltecController : Controller
+    {
+        public HoltecController() { }
+
+        // GET: Holtec
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome()
+        {
+            return Content("Welcome to Holtec");
+        }
+    }
+}
---
+++ 16July/16July/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_16July.MvcApplication" Language="C#" %>
