bd86152504af5c0926afad8dadad1e57763d16ef
Vishrut Aryan <VAryan@holtecasia.com>
2024-07-17 10:50:18 +0530

    mvc testing

---
+++ MVCMoApp/MVCMoApp/Controllers/ICICIController.cs
@@ -0,0 +1,36 @@
+﻿using MVCMoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMoApp.Controllers
+{
+    public class ICICIController : Controller
+    {
+        // GET: ICICI
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        public ActionResult Linkedin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(UserModel u1)
+        {
+            u1.SaveUser();
+            return RedirectToAction("Linkedin", "ICICI");
+        }
+    }
+}
---
+++ MVCMoApp/MVCMoApp/Models/UserModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MVCMoApp.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Username is Required")]
+
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is Required")]
+
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Email is Required")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Mobile Number is Required")]
+
+        public string Mobile { get; set; }
+
+        public void SaveUser() 
+        {
+            SqlConnection conn = new SqlConnection("Data Source = HA-NB69\\SQLEXPRESS;Initial Catalog = HOLTEC;User Id = sa;Password = 12345;");
+            conn.Open();
+
+            string query = "INSERT INTO USERDETAILS VALUES('" + UserName + "', '" + Password + "', '" + Email + "', '" + Mobile + "')";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
