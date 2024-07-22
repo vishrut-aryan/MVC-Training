a4511ef26252d4383cd75fdfbc256fea5a2e559d
Vishrut Aryan <VAryan@holtecasia.com>
2024-07-22 14:08:20 +0530

    code-first time

---
+++ CodeFirstExp/CodeFirstExp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodeFirstExp.MvcApplication" Language="C#" %>
---
+++ CodeFirstExp/CodeFirstExp/Models/CustomerContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstExp.Models
+{
+    public class CustomerContext
+    {
+    }
+
+    public class City 
+    {
+        [Key]
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public List<Customer> lstcst { get; set; }
+    }
+
+    public class Customer 
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerMobile { get; set; }
+        public int CityId { get; set; }
+        public City Citys { get; set; }
+    }
+}
