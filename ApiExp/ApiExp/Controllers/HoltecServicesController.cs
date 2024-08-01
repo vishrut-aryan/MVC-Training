b96cb31ae620bdd7e439e5a7a46b8144449c6d59
Vishrut Aryan <VAryan@holtecasia.com>
2024-08-01 11:00:03 +0530

    iytdrsedghjk hi

--- ApiExp/ApiExp/Controllers/HoltecServicesController.cs
+++ ApiExp/ApiExp/Controllers/HoltecServicesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiExp;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
 using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,15 +15,28 @@ namespace ApiExp.Controllers
         SqlConnection conn = new SqlConnection("Data Source = HA-NB69\\SQLEXPRESS;Initial Catalog = EFexp;User Id = sa;Password = 12345;TrustServerCertificate=true");
 
         // GET: api/<HoltecController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        [HttpGet("GetStates")]
+        public IEnumerable<StateDetails> Get()
         {
-            return new string[] { "value1", "value2" };
+            List<StateDetails> lststates = new List<StateDetails>();
+            SqlDataAdapter adap = new SqlDataAdapter("SELECT * FROM STATE", conn);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+                lststates.Add(new StateDetails
+                {
+                    StateID = (int)row["STATE_ID"],
+                    StateName = row["STATE_NAME"].ToString(),
+                    CountryID = (int)row["COUNTRY_ID"]
+                });
+            }
+            return lststates;
         }
 
         // Pass country id, return states
         // GET api/<HoltecController>/5
-        [HttpGet("{id}")]
+        [HttpGet("GetStatePerCountry/{id}")]
         public IEnumerable<StateDetails> Get(int id)
         {
             List<StateDetails> lststates = new List<StateDetails>();
@@ -41,36 +56,54 @@ namespace ApiExp.Controllers
         }
 
         // POST api/<HoltecController>
-        [HttpPost]
-        public void Post([FromBody] StateDetails state)
+        [HttpPost("AddState")]
+        public string Post([FromBody] StateDetails state)
         {
+            string str = "The state was added successfully";
+            int icount = 0;
             string query = "INSERT INTO STATE (STATE_NAME, COUNTRY_ID) VALUES (@StateName, @CountryID)";
-            using (SqlCommand cmd = new SqlCommand(query, conn))
-            {
-                cmd.Parameters.AddWithValue("@StateName", state.StateName);
-                cmd.Parameters.AddWithValue("@CountryID", state.CountryID);
-                conn.Open();
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@StateName", state.StateName);
+            cmd.Parameters.AddWithValue("@CountryID", state.CountryID);
+            conn.Open();
+
+            SqlCommand duplicheck = new SqlCommand("select count(*) from STATE where STATE_NAME='" + state.StateName + "'", conn);
+            icount = Convert.ToInt32(duplicheck.ExecuteScalar().ToString());
+            if (icount == 0)
                 cmd.ExecuteNonQuery();
-                conn.Close();
-            }
+            else
+                str = "This state already exists";
+
+            conn.Close();
+            return str;
         }
 
         // PUT api/<HoltecController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("UpdateState/{id}")]
+        public string Put(int id, [FromBody] StateDetails state)
         {
+            string query = "UPDATE STATE SET STATE_NAME = @StateName, COUNTRY_ID = @CountryID WHERE STATE_ID = @StateID";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@StateName", state.StateName);
+            cmd.Parameters.AddWithValue("@CountryID", state.CountryID);
+            cmd.Parameters.AddWithValue("@StateID", state.StateID);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return "The state was updated successfully";
         }
 
         // Service to delete a country by countryID
         // DELETE api/<HoltecController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("DeleteState/{id}")]
+        public string Delete(int id)
         {
             string query = "DELETE FROM STATE WHERE STATE_ID=" + id;
             SqlCommand cmd = new SqlCommand(query, conn);
             conn.Open();
             cmd.ExecuteNonQuery();
             conn.Close();
+            return "The state was deleted successfully";
         }
     }
 }
