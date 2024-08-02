7ed3fe3655102304a5630fab1905ca32d1334d23
Vishrut Aryan <VAryan@holtecasia.com>
2024-08-02 15:25:53 +0530

    hi

--- WebAPIClient/WebAPIClient/Controllers/HomeController.cs
+++ WebAPIClient/WebAPIClient/Controllers/HomeController.cs
@@ -18,6 +18,24 @@ namespace WebAPIClient.Controllers
             return View();
         }
 
+        public IActionResult Page1()
+        {
+            ViewBag.name = "index";
+            ViewData["email"] = "a@g.c";
+            TempData["mobile"] = "8976546782";
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+        public IActionResult Page3()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
@@ -27,6 +45,18 @@ namespace WebAPIClient.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ShowData()
+        {
+            Emp e1 = new Emp();
+            e1.EmpId = 1;
+            e1.EmpName = "samir";
+            Student s1 = new Student();
+            s1.RollNo = 401;
+            s1.StudName = "kapil";
+            ViewBag.s1 = s1;
+            return View(e1);
         }
     }
 }
---
+++ WebAPIClient/WebAPIClient/Models/EmpStudent.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPIClient.Models
+{
+    public class EmpStudent
+    {
+        public int EmpId { get; set; }
+        public string? EmpName { get; set; }
+        public int RollNo { get; set; }
+        public string? StudName { get; set; }
+    }
+    public class Emp
+    {
+        public int EmpId { get; set; }
+        public string? EmpName { get; set; }
+    }
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string? StudName { get; set; }
+    }
+}
